a743faabbf76c47789f5262b5afabc10ec0b9e0b
Shunsuke Kirino <Shunsuke.Kirino@access-company.com>
2013-06-07 02:25:19 +0900

    Suppress GTK's warning about tree_path == NULL

--- src/utils/tree_util.d
+++ src/utils/tree_util.d
@@ -47,8 +47,10 @@ TreePath GetPathAtPos(TreeView view, double x, double y)
   TreePath path;
   TreeViewColumn col;
   int cellx, celly;
-  view.getPathAtPos(cast(int)x, cast(int)y, path, col, cellx, celly);
-  return path;
+  if(view.getPathAtPos(cast(int)x, cast(int)y, path, col, cellx, celly)){
+    return path;
+  }
+  return null;
 }
 
 
