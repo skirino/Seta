772eae1603fb1749c58138a572cc140c0144243e
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2019-05-08 22:56:25 +0900

    Remove terminal_wrapper

--- src/page.d
+++ src/page.d
@@ -25,6 +25,7 @@ import std.algorithm;
 
 import gtk.VBox;
 import gtk.HBox;
+import gtk.VScrollbar;
 import gtk.Paned;
 import gtk.Widget;
 import gtk.Label;
@@ -39,7 +40,6 @@ import constants;
 import rcfile = config.rcfile;
 import config.page_init_option;
 import tab;
-import terminal_wrapper;
 import terminal;
 import mediator;
 
@@ -49,9 +49,7 @@ class Page : VBox
 private:
   Nonnull!Mediator mediator_;
   Nonnull!Tab      tab_;
-
-  Nonnull!Paned           paned_;
-  Nonnull!TerminalWrapper termWithScrollbar_;
+  Nonnull!Terminal term_;
 
   bool mapped_ = false;
 
@@ -72,17 +70,21 @@ public:
     super(0, 0);
     addOnMap(&ResetLayoutOnFirstMap);
 
-    tab_              .init(new Tab(side, ClosePage));
-    mediator_         .init(new Mediator(this));
-    termWithScrollbar_.init(new TerminalWrapper(mediator_, initialDir, opt.terminalRunCommand_, GetCWDFromMain));
-    mediator_.Set(termWithScrollbar_.Get());
-
-    paned_.init(new Paned(GtkOrientation.VERTICAL));
-    paned_.pack2(termWithScrollbar_, true, 0);
-    packStart(paned_, true, true, 0);
-
+    tab_     .init(new Tab(side, ClosePage));
+    mediator_.init(new Mediator(this));
+    term_    .init(new Terminal(mediator_, initialDir, opt.terminalRunCommand_, GetCWDFromMain));
+    mediator_.Set(term_);
+    PackTerminalWithScrollbar();
     showAll();
     SetLayout();
+  }
+
+  private void PackTerminalWithScrollbar() {
+    auto box = new HBox(false, 0);
+    box.packStart(term_, true, true, 0);
+    auto vscrollbar = new VScrollbar(term_.getVadjustment());
+    box.packStart(vscrollbar, false, false, 0);
+    packStart(box, true, true, 0);
   }
 
   void SetLayout() {
@@ -94,12 +96,7 @@ public:
         FilerMode();
       } else {
         BothMode();
-        paned_.setPosition(split);
       }
-    } else {
-      // Just realize all children on startup.
-      uint split = rcfile.GetWindowSizeV() / 2;
-      paned_.setPosition(split);
     }
   }
 
@@ -114,7 +111,7 @@ public:
   }
 
   bool     OnLeftSide () { return tab_.OnLeftSide(); }
-  Terminal GetTerminal() { return termWithScrollbar_.Get(); }
+  Terminal GetTerminal() { return term_; }
   Tab      GetTab     () { return tab_; }
 
   void CloseThisPage() {
@@ -160,32 +157,18 @@ public:
   }
 
 private:
-  void SetLastSplitPosition() {
-    uint pos = paned_.getPosition();
-    auto windowHeight = rcfile.GetWindowSizeV();
-    lastSplitPosition_ = max(min(pos, 9*windowHeight/10), windowHeight/10);
-  }
-
   void TerminalMode() {
     if(mode_ != ViewMode.TERMINAL) {
-      if(mode_ == ViewMode.BOTH) {
-        SetLastSplitPosition();
-      }
       mode_ = ViewMode.TERMINAL;
-      termWithScrollbar_.showAll();
       MoveFocusPosition();
     }
   }
 
   void FilerMode() {
     if(mode_ != ViewMode.FILER) {
-      if(mode_ == ViewMode.BOTH) {
-        SetLastSplitPosition();
-      }
       bool needUpdate = mode_ == ViewMode.TERMINAL;
       mode_ = ViewMode.FILER;
       MoveFocusPosition();
-      termWithScrollbar_.hide();
     }
   }
 
@@ -193,9 +176,7 @@ private:
     if(mode_ != ViewMode.BOTH) {
       bool needUpdate = mode_ == ViewMode.TERMINAL;
       mode_ = ViewMode.BOTH;
-      termWithScrollbar_.showAll();
       MoveFocusPosition();
-      paned_.setPosition(lastSplitPosition_);
     }
   }
   //////////////////////// view mode
@@ -221,23 +202,19 @@ public:
 
   ///////////////////////// manipulation of focus
   FocusInPage WhichIsFocused() {
-    if(termWithScrollbar_.Get().hasFocus()) {
-      return FocusInPage.LOWER;
-    } else {
-      return FocusInPage.NONE;
-    }
+    return FocusInPage.LOWER;
   }
 
   void FocusLower() {
-    termWithScrollbar_.Get().grabFocus();
+    term_.grabFocus();
   }
 
   void FocusUpper() {
-    termWithScrollbar_.Get().grabFocus();
+    term_.grabFocus();
   }
 
   void FocusShownWidget() {
-    termWithScrollbar_.Get().grabFocus();
+    term_.grabFocus();
   }
 
   void MoveFocusPosition() {
